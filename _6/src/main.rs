b8f8c66dd87bbe5fbeb4ffb11d9e722f0fcb91a6
Joao Pargana <jmpargana@gmail.com>
2024-12-06 16:58:36 +0000

    day 6 part 2

--- _6/src/main.rs
+++ _6/src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 use itertools::iproduct;
 use nom::{
     character::complete::{line_ending, one_of},
@@ -10,10 +12,10 @@ struct Puzzle {
     guard: Guard,
     rows: usize,
     cols: usize,
-    objects: Vec<(usize, usize)>,
+    objects: HashSet<(usize, usize)>,
 }
 
-#[derive(Debug, Clone, PartialEq)]
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
 enum Direction {
     Up,
     Down,
@@ -54,7 +56,7 @@ impl Puzzle {
     fn new(input: &str) -> Self {
         let (_, map) = parse_map(input).unwrap();
         let mut guard: Option<Guard> = None;
-        let mut objects = Vec::new();
+        let mut objects = HashSet::new();
 
         let guards = vec!['^', 'v', '<', '>'];
         for (x, y) in iproduct!(0..map.len(), 0..map[0].len()) {
@@ -62,7 +64,7 @@ impl Puzzle {
             let pt = (x, y);
 
             if cell == '#' {
-                objects.push((x, y));
+                objects.insert((x, y));
             }
 
             if guards.contains(&cell) {
@@ -87,30 +89,68 @@ impl Puzzle {
         }
     }
 
-    fn move_until_out(&mut self) -> usize {
-        let mut pos = Vec::new();
+    fn move_until_out(&self) -> usize {
+        let (pos, _) = self.has_cycle();
+        pos.iter().count() + 1
+    }
 
+    fn count_cycles(&self) -> usize {
+        let (pos, _) = self.has_cycle();
+        let mut cycles = HashSet::new();
+
+        for p in pos {
+            let mut objects = self.objects.clone();
+            objects.insert(p);
+            let np = Puzzle {
+                objects,
+                ..self.clone()
+            };
+            let (cycle, is_cycle) = np.has_cycle();
+            if is_cycle {
+                cycles.insert(cycle);
+            }
+        }
+        cycles.iter().count()
+    }
+
+    fn has_cycle(&self) -> (Vec<(usize, usize)>, bool) {
+        let mut puzzle = self.clone();
+        let mut pos = HashSet::new();
         loop {
-            if let Some((nx, ny)) = self.guard.walk() {
-                if nx >= self.rows || ny >= self.cols {
+            if let Some((nx, ny)) = puzzle.guard.walk() {
+                if pos.len() >= puzzle.rows * puzzle.cols {
+                    return (set_to_vec(pos), true);
+                }
+
+                if nx >= puzzle.rows || ny >= puzzle.cols {
                     break;
                 }
-                if self.objects.contains(&(nx, ny)) {
-                    self.guard.dir = self.guard.dir.rotate();
+                if puzzle.objects.contains(&(nx, ny)) {
+                    puzzle.guard.dir = puzzle.guard.dir.rotate();
                 } else {
-                    pos.push(self.guard.pt);
-                    self.guard.pt = (nx, ny);
+                    let np = (
+                        puzzle.guard.pt.0,
+                        puzzle.guard.pt.1,
+                        puzzle.guard.dir.clone(),
+                    );
+                    if !pos.insert(np) {
+                        return (set_to_vec(pos), true);
+                    }
+                    puzzle.guard.pt = (nx, ny);
                 }
             } else {
-                // walked less than 0
                 break;
             }
         }
-
+        let mut pos = set_to_vec(pos);
         pos.sort();
         pos.dedup();
-        pos.iter().count() + 1
+        (pos, false)
     }
+}
+
+fn set_to_vec(v: HashSet<(usize, usize, Direction)>) -> Vec<(usize, usize)> {
+    v.into_iter().map(|(x, y, _)| (x, y)).collect()
 }
 
 fn parse_map(input: &str) -> IResult<&str, Vec<Vec<char>>> {
@@ -119,32 +159,17 @@ fn parse_map(input: &str) -> IResult<&str, Vec<Vec<char>>> {
 
 fn main() {
     let input = include_str!("../puzzle.txt");
-    let distinct_pos = Puzzle::new(input).move_until_out();
+    let puzzle = Puzzle::new(input);
+    let distinct_pos = puzzle.move_until_out();
+    let cycles = puzzle.count_cycles();
 
     println!("result 1: {}", distinct_pos);
+    println!("result 2: {}", cycles);
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    #[test]
-    fn should_parse_input() {
-        let given = ".^.
-#..";
-
-        let expected = Puzzle {
-            guard: Guard {
-                pt: (0, 1),
-                dir: Direction::Up,
-            },
-            rows: 2,
-            cols: 3,
-            objects: vec![(1, 0)],
-        };
-        let actual = Puzzle::new(given);
-        assert_eq!(expected, actual);
-    }
 
     #[test]
     fn should_move_until_out() {
@@ -161,6 +186,58 @@ mod tests {
 
         let expected = 41;
         let actual = Puzzle::new(given).move_until_out();
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn should_have_cycle() {
+        let given = "....#.....
+.........#
+..........
+..#.......
+.......#..
+..........
+.#.#^.....
+........#.
+#.........
+......#...";
+
+        let (_, actual) = Puzzle::new(given).has_cycle();
+        assert!(actual);
+    }
+
+    #[test]
+    fn should_not_have_cycle() {
+        let given = "....#.....
+.........#
+..........
+..#.......
+.......#..
+..........
+.#..^.....
+........#.
+#.........
+......#...";
+
+        let (_, actual) = Puzzle::new(given).has_cycle();
+        assert!(!actual);
+    }
+
+    #[test]
+    fn should_have_n_cycles() {
+        let given = "....#.....
+.........#
+..........
+..#.......
+.......#..
+..........
+.#..^.....
+........#.
+#.........
+......#...";
+
+        let expected = 6;
+        let actual = Puzzle::new(given).count_cycles();
         assert_eq!(expected, actual);
     }
 }
