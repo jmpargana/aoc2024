5fc789e92124733159d5bb7c1af81c5c9ad020aa
Joao Pargana <jmpargana@gmail.com>
2024-12-08 22:27:35 +0000

    day 8 part 2

--- _8/src/main.rs
+++ _8/src/main.rs
@@ -48,6 +48,62 @@ impl Puzzle {
     fn count_unique_antinodes(&self) -> usize {
         self.unique_antinodes().iter().count()
     }
+
+    fn unique_continuous_antinodes(&self) -> Vec<(i32, i32)> {
+        let mut result = Vec::new();
+        for pts in self.pts.values() {
+            for pair in pts.iter().combinations(2) {
+                let [&a, &b] = pair[..] else { continue };
+                result.extend(continuous_antinodes(a, b, self.rows, self.cols));
+            }
+        }
+        result.sort();
+        result.dedup();
+        result
+    }
+
+    fn count_unique_continuous_antinodes(&self) -> usize {
+        self.unique_continuous_antinodes().iter().count()
+    }
+}
+
+fn continuous_antinodes(a: (i32, i32), b: (i32, i32), rows: usize, cols: usize) -> Vec<(i32, i32)> {
+    let (x0, y0) = (a.0, a.1);
+    let (x1, y1) = (b.0, b.1);
+
+    let mut dx = x1 - x0;
+    let mut dy = y1 - y0;
+
+    let gcd = gcd(dx.abs(), dy.abs());
+    dx /= gcd;
+    dy /= gcd;
+
+    let mut points = vec![a];
+
+    let mut current = (x1, y1);
+    while !is_within_bounds(current, rows, cols) {
+        points.push((current.0, current.1));
+        current.0 += dx;
+        current.1 += dy;
+    }
+
+    current = (x0 - dx, y0 - dy);
+    while !is_within_bounds(current, rows, cols) {
+        points.push((current.0, current.1));
+        current.0 -= dx;
+        current.1 -= dy;
+    }
+
+    points
+}
+
+fn gcd(mut a: i32, mut b: i32) -> i32 {
+    while b != 0 {
+        let temp = b;
+        b = a % b;
+        a = temp;
+    }
+    a
 }
 
 fn antinodes(a: (i32, i32), b: (i32, i32), rows: usize, cols: usize) -> Vec<(i32, i32)> {
@@ -68,7 +124,9 @@ fn main() {
     let map = include_str!("../puzzle.txt");
     let puzzle = Puzzle::new(map);
     let count = puzzle.count_unique_antinodes();
+    let count_continuous = puzzle.count_unique_continuous_antinodes();
     println!("result 1: {}", count);
+    println!("result 2: {}", count_continuous);
 }
 
 #[cfg(test)]
@@ -126,6 +184,51 @@ mod tests {
 
         let expected = 14;
         let actual = Puzzle::new(input).count_unique_antinodes();
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn should_generate_continuous_line() {
+        let expected = 5;
+        let actual = continuous_antinodes((0, 0), (2, 1), 10, 10).iter().count();
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn should_generate_example_part2_1() {
+        let input = "T.........
+...T......
+.T........
+..........
+..........
+..........
+..........
+..........
+..........
+..........";
+
+        let expected = 9;
+        let actual = Puzzle::new(input).count_unique_continuous_antinodes();
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn should_count_example_2() {
+        let input = "............
+........0...
+.....0......
+.......0....
+....0.......
+......A.....
+............
+............
+........A...
+.........A..
+............
+............";
+
+        let expected = 34;
+        let actual = Puzzle::new(input).count_unique_continuous_antinodes();
         assert_eq!(expected, actual);
     }
 }
